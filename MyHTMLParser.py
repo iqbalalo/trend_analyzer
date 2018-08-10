cda3480c0e17c2529652c078f95b1b4f24ff445f
Iqbal <iqbalalo@users.noreply.github.com>
2018-08-10 17:05:13 +0900

    Add meta keywords

--- MyHTMLParser.py
+++ MyHTMLParser.py
@@ -9,26 +9,36 @@ class MyHTMLParser(HTMLParser):
         self.end_tag = ""
         self.data = []
         self.hashtags = []
+        self.metakeywords = []
 
     def handle_starttag(self, tag, attrs):
         self.start_tag = tag
+        if tag == "meta":
+            if len(attrs)>=2 and 'keyword' in attrs[0] and 'content' in attrs[1]:
+                keys = self.clean_data(attrs[1][1])
+                self.metakeywords.extend(keys.split())
 
     def handle_endtag(self, tag):
         self.end_tag = tag
 
     def handle_data(self, data):
         if self.start_tag == self.end_tag and self.start_tag != "script":
-            data = data.strip()
-            data = re.sub("[(){}.,|>]", '', data)
-            data = " ".join(data.split())
+            data = self.clean_data(data)
             tags = re.findall("(#[A-Za-z0-9_-]+)", data)
             if tags:
                 self.hashtags.extend(tags)
             if data:
                 self.data.append(data)
+    
+    def clean_data(self, data):
+        data = data.strip()
+        data = re.sub("[(){}.,|>]", '', data)
+        data = " ".join(data.split())
+        return data
 
 # instantiate the parser and fed it some HTML
-# parser = MyHTMLParser()
-# parser.feed(requests.get("http://iqbalhossain.info").text)
+parser = MyHTMLParser()
+parser.feed(requests.get("http://iqbalhossain.info").text)
 # print(parser.data)
-# print(parser.hashtags)
+print(parser.hashtags)
+print(parser.metakeywords)
